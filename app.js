16a98eb588d23e21cfb7fb0f6d32c0fe45ae2373
AI Frontiers Server <jimzenn@aifrontiers.com>
2017-12-22 01:19:19 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const sassMiddleware = require('node-sass-middleware')
+const path = require('path')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname + '/source/scss'),
+    dest: path.join(__dirname + '/static'),
+    debug: true,
+    prefix: '/css'
+  })
+);
+
+app.use(express.static(path.join(__dirname, '/static')))
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
